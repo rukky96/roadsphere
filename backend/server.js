6c04f75deb83697db817f00d4758e99fcd466a7e
Akproko Kelvin <akprokokelvin@gmail.com>
2025-05-22 15:45:56 +0100

    Re-added backend as a regular folder

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+require('dotenv').config();
+const app = express(); 
+const port = process.env.PORT || 3333;
+app.use(express.static(path.join(__dirname, "../frontend")))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/home.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/login.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/register.html"))
+})
+
+app.get("/rentals", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/rentals.html"))
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/about.html"))
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/contact.html"))
+})
+
+app.get("/faq", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/faq.html"))
+})
+
+app.get("/explore", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/explore.html"))
+})
+
+app.listen(port, () => {
+    console.log("Server is running");
+})
+
+module.exports = app;
