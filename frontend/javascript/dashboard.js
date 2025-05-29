745099f814642e713e7ad1d099cb8c5f93d09707
Akproko Kelvin <akprokokelvin@gmail.com>
2025-05-29 11:35:32 +0100

    added stats box to the dashboard

--- frontend/javascript/dashboard.js
+++ frontend/javascript/dashboard.js
@@ -10,3 +10,33 @@ document.addEventListener("click", (e) => {
         sideBar.style.display = "none";
     }
 })
+
+  const userStats = document.querySelector('.user-stats');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  userStats.addEventListener('mousedown', (e) => {
+    isDown = true;
+    userStats.classList.add('dragging');
+    startX = e.pageX - userStats.offsetLeft;
+    scrollLeft = userStats.scrollLeft;
+  });
+
+  userStats.addEventListener('mouseleave', () => {
+    isDown = false;
+    userStats.classList.remove('dragging');
+  });
+
+  userStats.addEventListener('mouseup', () => {
+    isDown = false;
+    userStats.classList.remove('dragging');
+  });
+
+  userStats.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - userStats.offsetLeft;
+    const walk = (x - startX) * 2; // scroll speed
+    userStats.scrollLeft = scrollLeft - walk;
+  });
