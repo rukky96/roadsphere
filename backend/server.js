21b7c4644ed3f9fe06586538c3b71d6f4bb1b945
Akproko Kelvin <akprokokelvin@gmail.com>
2025-07-14 19:21:07 +0100

    allowed cors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const authRoutes = require("./auth_routes");
 const adminRoutes = require("./admin_routes");
 const apiRoutes = require("./api_routes")
 const swaggerSpec = require('./swagger');
+const cors = require("cors");
 require('dotenv').config();
 
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", apiRoutes);
 app.use("/api/admin", adminRoutes);
-
+app.use(cors());
 
 
 app.get('/api-docs', (req, res) => {
